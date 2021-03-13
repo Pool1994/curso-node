85351163a297a048690ffe8ee8ab56633f19cccc
Pool1994 <poolveracubas1994@gmail.com>
2021-03-13 17:30:48 -0500

    #

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const port = 8080;
+
+// motor de plantillas
+app.set("view engine", "ejs");
+
+app.set("views",__dirname + "/views");
+
+//middleware
+app.use(express.static(__dirname+"/public"));
+
+/** RUTAS */
+app.get("/",(req,res)=>{
+    res.render("home/index",{titulo:"titulo dinamico"});
+});
+
+
+
+/** sino encuentra la ruta, le arrojamos el error 404 */
+app.use((req,res,next)=>{
+    res.status(404).sendFile(__dirname+"/public/404.html");
+});
+app.listen(port,()=>{
+    console.log("Servicio a su puerto:"+port);
+});
