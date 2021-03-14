2e70afe2c6288818fa6c9e75335abc2d5b8bcb2f
Pool1994 <poolveracubas1994@gmail.com>
2021-03-13 20:32:53 -0500

    #

--- src/routers/mascotas.js
+++ src/routers/mascotas.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const Mascotas = require("../models/Mascotas");
-router.get("/",async(req,res)=>{
-    try{
-        const dataMascotas = Mascotas.find();
-        const result = await dataMascotas;
-        res.render("mascotas/index",{
-            data:result
-        }); 
-    }catch(e){
-        console.log(e);
-    }
+// const Mascotas = require("../models/Mascotas");
+router.get("/",(req,res)=>{
+    res.render("mascotas/index",{
+        data:[]
+    }); 
+
+    // try{
+    //     const dataMascotas = Mascotas.find();
+    //     const result = await dataMascotas;
+    //     res.render("mascotas/index",{
+    //         data:result
+    //     }); 
+    // }catch(e){
+    //     console.log(e);
+    // }
 });
 
 module.exports = router;
